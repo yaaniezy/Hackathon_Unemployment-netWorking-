fe7ed759be5bb134ac55cf3989450bcab795b8db
rayaan karlie <95684426+yaaniezy@users.noreply.github.com>
2022-02-22 13:41:46 +0200

    Delete store directory

--- store/index.js
+++
@@ -1,7 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import ThunkMiddleware from "redux-thunk";
-import rootReducer from "../reducers";
-
-const store = createStore(rootReducer, applyMiddleware(ThunkMiddleware));
-
-export default store;
